10dddb793447c27cd33fb5f0696cacaaadc522ab
Thomas James <tomsterlingjames@outlook.com.au>
2022-03-21 19:58:46 +1000

    Main.hbs and login.hbs

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,7 +3,8 @@ const { append } = require('express/lib/response');
 const sequelize = require('../config/connection');
 //const { Gallery, Painting } = require('../models');
 router.get('/', async (req, res) => {
-    return `Wrong route`
-});
+    
+        res.render('login');
+      });
 
 module.exports = router;
--- models/User.js
+++ models/User.js
@@ -16,7 +16,15 @@ User.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    name: {
+    // name: {
+    //   type: DataTypes.STRING,
+    //   allowNull: false,
+    // },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
       type: DataTypes.STRING,
       allowNull: false,
     },
