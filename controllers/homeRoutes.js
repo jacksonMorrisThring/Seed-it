d4d31db71bf8eb55c6261675707c799631860f2f
Hannah <94648556+hannahj25@users.noreply.github.com>
2022-03-24 20:22:08 +1030

    added myplants route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,6 +5,10 @@ router.get("/", async (req, res) => {
   res.render("landing");
 });
 
+router.get("/myplants", async (req, res) => {
+  res.render("myplants");
+});
+
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
