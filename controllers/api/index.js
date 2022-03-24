4aa2add5888780c9e22fdc8b29e4ed5cd22b6e01
Hannah <94648556+hannahj25@users.noreply.github.com>
2022-03-24 20:13:44 +1030

    imported api routes to controllers index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const apiRoutes = require('./api');
 
 router.use('/users', userRoutes);
+router.use('/api', apiRoutes);
 
 modules.exports = router;
